7c63f0a565a0d32c8fa409c3d7468a6f72873c88
investor228 <52128368+investor228@users.noreply.github.com>
2020-10-03 12:25:20 +0300

    feat: finish tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+}
+  for (let i=0;i<array.length;i++){
+  let tet = array.sort((a,b) => a-b);
+  return tet[0]; 
+  }
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+}
+  for (let i=0;i<array.length;i++){
+    let tet = array.sort((a,b) => b-a);
+    return tet[0];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+}
+  for (let i=0;i<array.length;i++){
+   let tet = array.reduce((a,b) => (a+b)) / array.length;
+   return tet;
+  }
 }
